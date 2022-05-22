4eca1148ecc71aa96bec916716b6f6ab8c4d9ed3
James Schwartz <jbschwartz@users.noreply.github.com>
2022-05-22 09:25:44 -0400

    Get a file reading and writing.

---
+++ examples/example_1.s
@@ -0,0 +1,2 @@
+add x9, x20, x21
+add x0, x0, x0
--- riscv_asm/__main__.py
+++ riscv_asm/__main__.py
@@ -1,1 +1,20 @@
-print("Hello world")
+import logging
+import os
+import sys
+
+logger = logging.getLogger("riscv_asm")
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        logger.error("Please provide the file to convert via command line argument.")
+        sys.exit()
+
+    input_file_location = sys.argv[1]
+
+    output_file_name = os.path.basename(input_file_location).split(".")[0] + ".o"
+    output_file_location = f"./{output_file_name}"
+
+    with open(input_file_location, "r", encoding="utf-8") as input_file:
+        with open(output_file_location, "wb") as obj_file:
+            for index, string in enumerate(input_file):
+                obj_file.write(bytes([ord(s) for s in string]))
